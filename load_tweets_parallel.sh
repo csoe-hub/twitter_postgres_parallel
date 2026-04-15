4fac39017dbb9e8fba25cba6b4e42701b764f5a6
Carter Soe <csoe@students.pitzer.edu>
2026-04-15 04:22:36 -0700

    Match working CI setup from reference repo

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -1,22 +1,16 @@
 #!/bin/sh
 
-files=$(find data/*)
-
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time echo "$files" | parallel ./load_denormalized.sh
+time ls data/* | parallel sh load_denormalized.sh {}
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time echo "$files" | parallel 'python3 ./load_tweets.py \
-  --db postgresql://postgres:pass@localhost:21002/postgres \
-  --inputs "{}"'
+time ls data/* | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:21002/postgres --inputs={}
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time echo "$files" | parallel 'python3 ./load_tweets_batch.py \
-  --db postgresql://postgres:pass@localhost:21003/postgres \
-  --inputs "{}"'
+time ls data/* | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:21003/postgres --inputs={}
